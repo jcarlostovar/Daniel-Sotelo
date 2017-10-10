e22951fa2f75eaf01cda62b953eb8ebe860fbe1e
Juan Carlos Tovar L <30672753+jcarlostovar@users.noreply.github.com>
2017-10-10 10:03:44 -0500

    Cambios de Formulario
    Se realizan cambios en formulario de Cotiza y en el PHP.

--- enviar.php
+++ enviar.php
@@ -1,4 +1,5 @@
 <?php
+
     $destino = "meren69@hotmail.com";
     $nombre = $_POST["nombre"];
     $tel = $_POST["tel"];
@@ -6,15 +7,26 @@
     $ciudad = $_POST["ciudad"];
     $estado = $_POST["estado"];
     $evento = $_POST["evento"];
-    $contenido = "Nombre: ".$nombre.
-                "\nTelefono: ".$tel.
-                "\nCorreo: ".$email.
-                "\nCiudad: ".$ciudad.
-                "\nEstado: ".$estado.
-                "\nTipo de Evento: ".$Evento;
-    mail($destino,"Solicitud de Cotizacion",$contenido);
-    header("Location:cotiza.html");
+    $contenido = "
+            Nombre: $_POST[nombre]
+            Telefono: $_POST[tel]
+            Correo: $_POST[email]
+            Ciudad: $_POST[ciudad]
+            Estado: $_POST[estado]
+            Tipo de Evento: $_POST[evento]";
 
+    $headers ='From:'.$_POST['email']."\r\n".
+              'Reply-To:'.$_POST['email']."\r\n".
+              'X-Mailer:PHP/' . phpversion();
 
+              //  "\nCorreo: ".$email.
+              //  "\nCiudad: ".$ciudad.
+              //  "\nEstado: ".$estado.
+              //  "\nTipo de Evento: ".$Evento;
+
+    mail($destino,"Solicitud de Cotizacion",$contenido,$headers);
+    //header("Location:cotiza.html");
+
+    echo "Mensaje Enviado";
 
  ?>
